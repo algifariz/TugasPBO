d2ee0ee5522b348513af3f5adae399bd01f319a2
Moch algifari AZ <93359682+algifariz@users.noreply.github.com>
2021-11-16 10:34:28 +0700

    Create battrey.php

---
+++ battrey.php
@@ -0,0 +1,30 @@
+<?php
+/* APLIKASI PERHITUNGAN BATTRERY */
+class Battery {
+    public $full;
+    protected $desain;
+
+    public function __construct($full, $desain) {
+        $this->full = $full;
+        $this->desain = $desain;
+
+    }
+} 
+
+class hitung extends battery {
+    public function hitung_battery(){
+        $x = round(($this->full/$this->desain))*100;
+        return $x."%";
+    }
+
+    public function __destruct(){
+        echo "keterangan:<br>
+        >=60% = Masih Baik <br>
+        <60% = Saatnya ganti<br>";
+
+    }
+}
+
+$hasil = new hitung (37,34);
+
+echo "battery percentase hasil adalah=".$hasil->hitung_battery()."<hr>"
